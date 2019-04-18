1030b7c91139cd20022264f8e7dba05673e58841
Oxibug <agskitkat@gmail.com>
2019-04-18 17:50:06 +0300

    Calendar progress 20%

--- js/app.js
+++ js/app.js
@@ -20,10 +20,48 @@ app.controller("pageController", function($scope) {
         d:0,
         hs:0,
         he:0,
-        mounth: [
+        offset:0,
+        month: [
               
         ]
     };
+
+    function getDays(y, m) {
+        var days = [];
+        var days_in_month = 32 - new Date(y, m, 32).getDate();
+        var date = new Date(y, m,1);
+        var day = date.getDay();
+        if (day === 0) { // день 0 становится 7
+            day = 7;
+        }
+
+        day = day -2;
+
+        for(var i = 0;i <= (day + days_in_month); i++) {
+
+            if(i <= day ) {
+                days[i] = {
+                    weekend:false
+                };
+            } else {
+                var md = (i - day);
+                var wd = new Date(y, m, md).getDay();
+                if (wd === 0) { // день 0 становится 7
+                    wd = 7;
+                }
+                days[i] = {
+                    num: md,
+                    weekend: (wd <= 5) ? false:true
+                };
+            }
+
+        }
+
+        console.log(days);
+        $scope.pickerDateTime.month = days;
+    }
+
+    getDays(2019, 3);
 
     $scope.order = {
         sauna: {
