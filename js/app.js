c38bcd4a446576e70364c66e413e4294008c73b1
Oxibug <agskitkat@gmail.com>
2019-04-24 17:57:12 +0300

    Summary

--- js/app.js
+++ js/app.js
@@ -4,37 +4,65 @@ var app = angular.module("app-sauna", ['slick']);
 app.factory('tablesheet', function(){
     return {
         get: function() {
-            
+            {
+
+            }
         }
     }
 });
 
+app.factory('sauna-list', function(){
+    return {
+        get: function() {
+            return [
+                {
+                    id: "1",
+                    name: "Сауна №1",
+                    price: 1499,
+                    min_h: 3,
+                    max_guest:4,
+                    guest_overprice:100,
+                    options: [
+                        "Простыня","Полотенце","Тапочки","Джакузи","Душ","Купель: 5х5 м",
+                        "Парная: 15 кв.м","Зона отдыха: 30 кв.м","Комната отдыха","TV"
+                    ],
+                }
+            ]
+        }
+    }
+});
 
 app.factory('services', function(){
     return {
         get: function() {
             return [
                 {
+                    id: "1",
                     name: "Банщик",
                     services: [
                         {
+                            id: "1",
                             name: "Парение классическое",
                             time: "30 мин",
                             price: 4499
                         },{
+                            id: "2",
                             name: "Парение",
                             time: "45 мин",
                             price: 1499
                         }
                     ]
                 },{
+                    id: "2",
                     name: "Массажист",
                     services: [
                         {
+                            id: "1",
                             name: "Парение классическое",
                             time: "30 мин",
                             price: 4499
                         },{
+                            id: "2",
                             name: "Парение",
                             time: "45 мин",
                             price: 1499
@@ -51,24 +79,81 @@ app.controller("pageController", function($scope) {
     $scope.view_content = true;
     $scope.view_pages = false;
 
+    $scope.current_sauna = {
+        id: 1,
+        datetime:{
+            start:0,
+            stop:0
+        },
+        guest:1,
+        price:0
+    };
+
     $scope.order = {
         sauna: {
-            object: "sauna-n-1",
-            datetime:"дата и время аренды",
-            guest:1,
+
         },
         service: [
             {
-                id: "service1"
+                id: 1,
+                price:2499,
+                services: [
+                    {
+                        id: 1,
+                        price: 2499
+                    },{
+                        id: 2,
+                        price: 2499
+                    }
+                ]
             }
         ],
-        store: [
-            {id: 1, count: 3},
-            {id: 2, count: 2},
-            {id: 3, count: 1}
-        ],
+        store: [{
+                id: 1,
+                count: 3,
+                price: 2499
+            },{
+                id: 2,
+                count: 2,
+                price: 2499
+            },{
+                id: 3,
+                count: 1,
+                price: 2499
+        }],
         sum: 100500
     };
+
+
+    $scope.getServiceName = function(id){
+
+    };
+
+
+    $scope.calcOrderSum = function() {
+        // Calc sauna
+    };
+
+    // SAUNA
+    $scope.showChooseSauna = function () {
+
+    };
+    $scope.chooseSauna = function () {
+
+    };
+    $scope.showDatePicker = function () {
+
+    };
+    $scope.saunaMansCount = function(derection) {
+        if( ($scope.order.sauna.guest + derection)  >= 1) {
+
+        }
+    };
+    $scope.buttonOrderSauna = function() {
+
+    };
+
+
 
     $scope.viewPage = function(view_page) {
         $scope.view_page = view_page;
